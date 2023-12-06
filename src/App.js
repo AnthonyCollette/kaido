eea137804ddc2269febe2911e5ae25b2b2a63f7a
Anthony Collette <100pour100inde@gmail.com>
2023-12-06 10:38:20 +0100

    add emailjs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './assets/scss/main.scss';
 import Homepage from './pages/Homepage';
 import Contact from './pages/Contact';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import { Helmet } from 'react-helmet';
 import Error404 from './pages/Error404';
 import AboutPage from './pages/AboutPage';
@@ -15,13 +15,12 @@ import { useTranslation } from 'react-i18next'
 function App() {
 
   const { t } = useTranslation()
-
+  
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <ScrollToTop />
         <Helmet>
-          <meta charSet='utf-8' />
           <title>{t('Site.title')}</title>
           <meta name="description" content={t('Site.desc')} />
         </Helmet>
@@ -35,7 +34,7 @@ function App() {
           <Route path="*" element={<Error404 />} />
         </Routes>
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,49 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useIsVisible } from '..';
 import { useTranslation } from 'react-i18next';
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
-
     const contentRef = useRef()
     const contentRefIsVisible = useIsVisible(contentRef)
     const { t } = useTranslation()
 
+    const [success, setSuccess] = useState(false)
+    const [error, setError] = useState(false)
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log(process.env)
+        emailjs.sendForm(process.env.REACT_APP_SERVICEID, process.env.REACT_APP_TEMPLATEID, contentRef.current, process.env.REACT_APP_PUBLICKEY)
+            .then((result) => {
+                setSuccess(true)
+            }, (error) => {
+                setError(true)
+            });
+    };
+
     return (
-        <form ref={contentRef}>
-            <div className={contentRefIsVisible ? 'form-group fg-input slideInBottom' : 'form-group fg-input'}>
-                <input type='text' name='lastname' placeholder={t('Contact-Page.name')}></input>
-            </div>
-            <div className={contentRefIsVisible ? 'form-group fg-input slideInBottom' : 'form-group fg-input'}>
-                <input type='text' name='firstname' placeholder={t('Contact-Page.firstname')}></input>
-            </div>
-            <div className={contentRefIsVisible ? 'form-group fg-input slideInBottom' : 'form-group fg-input'}>
-                <input type='text' name='email' placeholder={t('Contact-Page.mail')}></input>
-            </div>
-            <div className={contentRefIsVisible ? 'form-group fg-textarea slideInBottom' : 'form-group fg-textarea'}>
-                <textarea type='text' name='message' placeholder={t('Contact-Page.message')}></textarea>
-            </div>
-            <button className={contentRefIsVisible ? 'btn btn--primary slideInBottom' : 'btn btn--primary'}>
-                {t('Contact-Page.button')}
-            </button>
-        </form>
+        <>
+            <form ref={contentRef} onSubmit={sendEmail} className={error || success ? 'display-none' : ''}>
+                <div className={contentRefIsVisible ? 'form-group fg-input slideInBottom' : 'form-group fg-input'}>
+                    <input type='text' name='user_lastname' placeholder={t('Contact-Page.name')} required></input>
+                </div>
+                <div className={contentRefIsVisible ? 'form-group fg-input slideInBottom' : 'form-group fg-input'}>
+                    <input type='text' name='user_firstname' placeholder={t('Contact-Page.firstname')} required></input>
+                </div>
+                <div className={contentRefIsVisible ? 'form-group fg-input slideInBottom' : 'form-group fg-input'}>
+                    <input type='email' name='user_email' placeholder={t('Contact-Page.mail')} required></input>
+                </div>
+                <div className={contentRefIsVisible ? 'form-group fg-textarea slideInBottom' : 'form-group fg-textarea'}>
+                    <textarea type='text' name='message' placeholder={t('Contact-Page.message')} required></textarea>
+                </div>
+                <button type='submit' className={contentRefIsVisible ? 'btn btn--primary slideInBottom' : 'btn btn--primary'}>
+                    {t('Contact-Page.button')}
+                </button>
+            </form>
+            {success && <p className='success'>{t('Contact-Page.success')}</p>}
+            {error && <p className='error'>{t('Contact-Page.error')}</p>}
+        </>
     );
 };
 
